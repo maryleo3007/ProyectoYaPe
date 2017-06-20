69b8bf1b2af3fa0fef059a61aa61baae01be78d6
Janine Vega <osmary.vega@gmail.com>
2017-06-20 15:42:26 -0500

    commit before merge screen1

---
+++ public/js/components/validarNumeroScreenTwo.js
@@ -0,0 +1,8 @@
+const screenValidarNumero = (update) => {
+  const container = $('<div class="container"></div>');
+  const row = $('<div class="row containerInicio"></div>');
+  const col = $('<div class="col-md-12 col-sm-12"></div>');
+
+
+  return container
+}
